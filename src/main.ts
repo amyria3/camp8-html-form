fc97128907d6ca8b42ca5fa304c0ba422d352106
Franz Wollang <franzwollang@gmail.com>
2022-12-15 17:19:18 +0100

    form submit handler added

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 export {};
+
+const form = document.getElementById("form") as HTMLFormElement;
+
+// when a button with type "submit" is clicked, the form the button is in emits the "submit" event
+
+form.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	const formData = new FormData(form);
+	console.log(Object.fromEntries(formData.entries()));
+});
